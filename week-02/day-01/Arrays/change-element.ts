c9afe5997097c2084a79e72993e27e7e1148a82e
Lhaktoz <andi.horvath2@gmail.com>
2020-11-23 16:14:05 +0100

    added arrays exercises

---
+++ week-02/day-01/Arrays/change-element.ts
@@ -0,0 +1,11 @@
+'use strict';
+// -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+// let numbers: number[] = [1, 2, 3, 8, 5, 6];
+
+ 
+// let roots =numbers.map();
+
+// console.log(roots);
