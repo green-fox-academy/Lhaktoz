fcb01205e605406d50a021ad1df346209397a846
Lhaktoz <andi.horvath2@gmail.com>
2020-11-24 15:07:11 +0100

    url exercise

---
+++ week-02/day-02/strings/url-fixer.ts
@@ -0,0 +1,17 @@
+  
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+
+let replacedUrl: string = url.replace('bots','odds');
+let firstPartUrl: string = replacedUrl.slice(0,4);
+let lastPartUrl: string = replacedUrl.slice(5);
+// console.log(firstPartUrl);
+// console.log(lastPartUrl);
+firstPartUrl = firstPartUrl +':'
+let newUrl: string = firstPartUrl.concat(lastPartUrl);
+
+console.log(newUrl)
+
