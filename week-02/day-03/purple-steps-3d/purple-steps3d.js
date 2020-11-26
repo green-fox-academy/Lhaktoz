fb3b3dabfcc5b326e19a46c332ea3a834f024398
Lhaktoz <andi.horvath2@gmail.com>
2020-11-26 12:36:25 +0100

    solved purplesteps3d

--- week-02/day-03/purple-steps-3d/purple-steps3d.js
+++ week-02/day-03/purple-steps-3d/purple-steps3d.js
@@ -3,10 +3,11 @@
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
-for ( let i = 1 ; i < 6 ; i++){
-  ctx.fillStyle = 'purple';
-  let size = 10;
-  ctx.fillRect(0 + (i * size), i +(i * size) , size, size);
-
+let sumsize = 10;
+for ( let i = 0 ; i < 6 ; i++){
+  let size = 10 * (i+1);
+   ctx.fillStyle = 'purple';
+   ctx.fillRect( sumsize, sumsize , size , size);
+   sumsize =  sumsize + size; 
 }
 
--- week-02/day-03/purple-steps/purple-steps.js
+++ week-02/day-03/purple-steps/purple-steps.js
@@ -2,3 +2,11 @@
 
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
+
+
+for ( let i = 0 ; i < 30 ; i++){
+  let size = 10;
+  ctx.fillStyle = 'purple';
+  ctx.fillRect( size +(i * size), size +(i * size ) , size, size);
+
+}
