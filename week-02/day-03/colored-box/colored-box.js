67e6928c63307a9003c9671117df5b3a195cb95c
Lhaktoz <andi.horvath2@gmail.com>
2020-11-30 18:24:01 +0100

    colored box

--- week-02/day-03/colored-box/colored-box.js
+++ week-02/day-03/colored-box/colored-box.js
@@ -32,30 +32,33 @@ const ctx = canvas.getContext('2d');
 
 
 
-// ctx.strokeStyle = 'red'
-// ctx.beginPath();
-// ctx.moveTo(200,100);
-// ctx.lineTo(400,100);
-// ctx.stroke();
-// ctx.strokeStyle = 'yellow'
-// ctx.lineTo(400,200);
-// ctx.stroke();
-// ctx.strokeStyle = 'blue'
-// ctx.lineTo(200,200);
-// ctx.stroke();
-// ctx.strokeStyle = 'green'
-// ctx.lineTo(200,100);
-// ctx.stroke();
 
-function drawlinewithcolor(x,y,xx,yy, color){
+
+function drawLineWithColor(x,y,xx,yy, color){
   ctx.strokeStyle = color;
   ctx.beginPath();
   ctx.moveTo(x,y);
   ctx.lineTo(xx,yy);
   ctx.stroke();
-}
-drawlinewithcolor(200,100,400,100,'red');
-drawlinewithcolor(400,100,400,200,'green');
-drawlinewithcolor(400,200,200,200,'blue');
-drawlinewithcolor(200,200,200,100,'yellow');
+ };
+// drawLineWithColor(200,100,400,100,'red');
+// drawLineWithColor(400,100,400,200,'green');
+// drawLineWithColor(400,200,200,200,'blue');
+// drawLineWithColor(200,200,200,100,'yellow');
+
+
+
+
+
+
+function drawColoredBox(beginX,beginY, width, height, color1, color2, color3, color4) {
+  drawLineWithColor(beginX, beginY, beginX + width, beginY, color1);
+  drawLineWithColor(beginX + width, beginY, beginX + width, beginY + height, color2);
+  drawLineWithColor(beginX, beginY + height , beginX + width, beginY + height , color3);
+  drawLineWithColor(beginX, beginY + height, beginX , beginY, color4);
+};
+
+drawColoredBox(250, 200, 60, 20, 'yellow', 'darkgreen', 'orange', 'darkgrey');
+
+
 
