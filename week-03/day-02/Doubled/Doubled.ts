d11ec985c7d394bb9745991a3c27a8339f5ef9ea
Lhaktoz <andi.horvath2@gmail.com>
2020-12-12 15:02:08 +0100

    correcting style

--- week-03/day-02/Doubled/Doubled.ts
+++ week-03/day-02/Doubled/Doubled.ts
@@ -1,17 +1,16 @@
 'use strict';
 
+ const fs = require('fs');
 
-  const fs = require('fs');
-  try {
-    let fileContent = fs.readFileSync('Doubled.txt', 'utf-8'); 
-    let fileArray = fileContent.split('');
-    var newarr = [fileArray[0]];
-    for (var i = 1; i < fileArray.length; i++) {
-       if (fileArray[i]!=fileArray[i-1]) newarr.push(fileArray[i]);
-    }
-    
-    let newFileContent = newarr.join('')
-    console.log(newFileContent)
+ try {
+  let fileContent = fs.readFileSync('Doubled.txt', 'utf-8'); 
+  let fileArray = fileContent.split('');
+  var newarr = [fileArray[0]];
+  for (var i = 1; i < fileArray.length; i++) {
+     if (fileArray[i]!=fileArray[i-1]) newarr.push(fileArray[i]);
+  }
+  let newFileContent = newarr.join('')
+  console.log(newFileContent)
   } catch (e) {
     console.log(e);
   }
