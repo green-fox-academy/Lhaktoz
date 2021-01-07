f2264073be04d027c79d92f1e08c2f74d070fed6
Lhaktoz <andi.horvath2@gmail.com>
2021-01-07 21:59:31 +0100

    started todo

--- week-06/day-04/greetings/server.js
+++ week-06/day-04/greetings/server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// home page
 app.get('/', (req, res) => {
   let name = req.query.name;
   if (name == undefined) {
@@ -15,9 +13,6 @@ app.get('/', (req, res) => {
     }
 });
 
-
-
-// start express app on port 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
