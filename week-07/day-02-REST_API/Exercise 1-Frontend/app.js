f4ce179cc569d396c264b868a531945b0cd80c08
Lhaktoz <andi.horvath2@gmail.com>
2021-01-12 15:48:17 +0100

    greeting done

--- week-07/day-02-REST_API/Exercise 1-Frontend/app.js
+++ week-07/day-02-REST_API/Exercise 1-Frontend/app.js
@@ -15,7 +15,20 @@ app.get('/doubling', (req, res) => {
     res.status(200).json({"error": "Please provide an input!"})
   } else {
   res.json({"received": Number(req.query.input), "result": Number(req.query.input*2)});
-}
+    }
 });
+
+app.get('/greeter', (req, res) => {
+  if(!req.query.name && !req.query.title){
+    res.status(400).json({"error": "Please provide a name and a title!"})
+  }else if(!req.query.name) {
+    res.status(400).json({"error": "Please provide a name!"})
+   } else if(!req.query.title){
+    res.status(400).json({"error": "Please provide a title!"})
+   }
+   else {
+    res.json({"welcome_message": `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`})
+    }
+  });
 
 app.listen(3000);
