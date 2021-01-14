cc8ac852ccbb58a2c50a057d7af12c97fbc40166
Lhaktoz <andi.horvath2@gmail.com>
2021-01-14 18:27:26 +0100

    started filter

--- week-07/day-04-NodeJS_MySQL/bookstore/server.js
+++ week-07/day-04-NodeJS_MySQL/bookstore/server.js
@@ -19,7 +19,8 @@ conn.connect((err) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('működik')
 })
 
 
@@ -30,8 +31,49 @@ app.get('/books', (req,res) => {
       res.status(500).json({'error': 'database error'});
       return;
     }
-    res.send(rows);
+    res.json(rows);
   });
 });
+
+app.get('/booksdetails', (req,res) => {
+  let longquery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM  book_mast 
+  INNER JOIN author ON book_mast.aut_id = author.aut_id 
+  INNER JOIN category ON book_mast.cate_id = category.cate_id 
+  INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`
+
+  conn.query(longquery, (err, rows) => {
+    if(err) {
+      console.log(err.toString());
+      res.status(500).json({'error': 'database error'});
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+
+app.get('/booksdetails:filter', (req,res) => {
+
+
+  let longquery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+  FROM  book_mast 
+  INNER JOIN author ON book_mast.aut_id = author.aut_id 
+  INNER JOIN category ON book_mast.cate_id = category.cate_id 
+  INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id
+  WHERE cate_descript = (?);`
+
+  conn.query(longquery, [req.params.filter], (err, rows) => {
+    if(err) {
+      console.log(err.toString());
+      res.status(500).json({'error': 'database error'});
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+
+
 
 app.listen(3000);
