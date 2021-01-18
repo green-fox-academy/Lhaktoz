ba4f0dc38cba997355c67e695aa3bf77bf069693
Lhaktoz <andi.horvath2@gmail.com>
2021-01-18 18:16:38 +0100

    rename folders

--- week-07/day-04-NodeJS_MySQL/bookstore/server.js
+++ week-07/day-04-NodeJS_MySQL/bookstore/server.js
@@ -5,12 +5,14 @@ const app = express();
 const mysql = require('mysql');
 
 app.use(express.json());
+app.use(express.static('public'));
 
-let conn = mysql.createConnection({
+const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'bookstore'
+  database: 'bookstore',
+  insecureAuth: 'true',
 });
 
 conn.connect((err) => {
@@ -19,12 +21,11 @@ conn.connect((err) => {
 });
 
 app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'index.html'));
-  res.send('működik')
+  res.sendFile(path.join('index.html'));
 })
 
 
-app.get('/books', (req,res) => {
+app.get('/booktitles', (req,res) => {
   conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
     if(err) {
       console.log(err.toString());
@@ -35,45 +36,45 @@ app.get('/books', (req,res) => {
   });
 });
 
-app.get('/booksdetails', (req,res) => {
-  let longquery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
-  FROM  book_mast 
-  INNER JOIN author ON book_mast.aut_id = author.aut_id 
-  INNER JOIN category ON book_mast.cate_id = category.cate_id 
-  INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`
-
-  conn.query(longquery, (err, rows) => {
-    if(err) {
-      console.log(err.toString());
-      res.status(500).json({'error': 'database error'});
-      return;
-    }
-    res.json(rows);
-  });
-});
 
 
-app.get('/booksdetails:filter', (req,res) => {
-
+app.get('/books', (req,res) => {
+  let category = '';
+  let publisher = '';
+  let plt = 0;
+  let pgt = 0;
 
   let longquery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
   FROM  book_mast 
   INNER JOIN author ON book_mast.aut_id = author.aut_id 
   INNER JOIN category ON book_mast.cate_id = category.cate_id 
   INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id
-  WHERE cate_descript = (?);`
+  WHERE cate_descrip LIKE (?)
+  AND pub_name LIKE (?);`;
+  // AND book_price < (?)
+  // AND book_price > (?);`;
 
-  conn.query(longquery, [req.params.filter], (err, rows) => {
+  if (!req.query.category) {
+    category = '%';
+  } else {
+    category = req.query.category;
+  }
+
+  if(!req.query.publisher) {
+    publisher = '%';
+  } else {
+    publisher = req.query.publisher;
+  }
+
+  conn.query(longquery, [category, publisher], (err, rows) => {
     if(err) {
       console.log(err.toString());
       res.status(500).json({'error': 'database error'});
       return;
     }
-    res.json(rows);
+    res.json(rows); 
   });
 });
-
-
 
 
 app.listen(3000);
