a9782449a4fed77cfadeeac210212d3a8faf6043
Lhaktoz <andi.horvath2@gmail.com>
2021-02-03 14:33:47 +0100

    added server file, books endpoint

--- week-07/day-04-NodeJS_MySQL/bookstore/server.js
+++ week-07/day-04-NodeJS_MySQL/bookstore/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
+const path = require('path');
+
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -21,60 +23,20 @@ conn.connect((err) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join('index.html'));
-})
+  res.sendFile(path.join('index.html'))
+});
 
-
-app.get('/booktitles', (req,res) => {
-  conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
+app.get('/books', (req, res) => {
+  conn.query('SELECT book_name FROM book_mast', (err, rows) => {
     if(err) {
-      console.log(err.toString());
+      // console.log(err.toString());
       res.status(500).json({'error': 'database error'});
       return;
     }
-    res.json(rows);
+    res.status(200).json(rows);
   });
 });
 
-
-
-app.get('/books', (req,res) => {
-  let category = '';
-  let publisher = '';
-  let plt = 0;
-  let pgt = 0;
-
-  let longquery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
-  FROM  book_mast 
-  INNER JOIN author ON book_mast.aut_id = author.aut_id 
-  INNER JOIN category ON book_mast.cate_id = category.cate_id 
-  INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id
-  WHERE cate_descrip LIKE (?)
-  AND pub_name LIKE (?);`;
-  // AND book_price < (?)
-  // AND book_price > (?);`;
-
-  if (!req.query.category) {
-    category = '%';
-  } else {
-    category = req.query.category;
-  }
-
-  if(!req.query.publisher) {
-    publisher = '%';
-  } else {
-    publisher = req.query.publisher;
-  }
-
-  conn.query(longquery, [category, publisher], (err, rows) => {
-    if(err) {
-      console.log(err.toString());
-      res.status(500).json({'error': 'database error'});
-      return;
-    }
-    res.json(rows); 
-  });
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
 });
-
-
-app.listen(3000);
